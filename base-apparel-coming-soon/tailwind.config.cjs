9eaf35e096106a92d83107ec93f6c85eb1af3ca3
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-08-25 23:22:25 -0500

    finished desktop version and made some changes on how to display part of the elements

--- base-apparel-coming-soon/tailwind.config.cjs
+++ base-apparel-coming-soon/tailwind.config.cjs
@@ -2,16 +2,16 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		screens: {
+			'mobile': '375px',
+			// => @media (min-width: 375px) { ... }
+			'desktop': '1440px',
+			// => @media (min-width: 1440px) { ... }
+		},
 		extend: {
 			fontFamily: {
 				'Josefin Sans': ['Josefin Sans', "sans-serif"],
 			},
-			screens: {
-				'mobile': '375px',
-				// => @media (min-width: 375px) { ... }
-				'desktop': '1440px',
-				// => @media (min-width: 1280px) { ... }
-			}
 		},
 	},
 	plugins: [],

0e897edf875344011c8bf543ff0b815ace0bff0f
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-09-16 22:31:14 -0500

    first commit for planets-fact-site

---
+++ planets-fact-site/astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import image from "@astrojs/image";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind({
+    // Example: Disable injecting a basic `base.css` import on every page.
+    // Useful if you need to define and/or import your own custom `base.css`.
+    config: { applyBaseStyles: false },
+  }), image()]
+});

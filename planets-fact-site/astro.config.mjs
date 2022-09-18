eba0f1f16809c07c9b591a9018a20d9fcabff9fd
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-09-18 13:03:17 -0500

    added venus routing

--- planets-fact-site/astro.config.mjs
+++ planets-fact-site/astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,5 +7,5 @@ export default defineConfig({
     // Example: Disable injecting a basic `base.css` import on every page.
     // Useful if you need to define and/or import your own custom `base.css`.
     config: { applyBaseStyles: false },
-  }), image()]
+  })]
 });

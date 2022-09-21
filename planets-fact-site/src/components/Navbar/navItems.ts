ec0035c6c2e4ed789425039ffbca2ddc0b454362
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-09-21 18:50:10 -0500

    navbar refactored

---
+++ planets-fact-site/src/components/Navbar/navItems.ts
@@ -0,0 +1,44 @@
+export const navItems = [
+    {
+      name: "VENUS",
+      color: "decoration-[#EDA249]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#EDA249] hover:underline-offset-[-45px]",
+    },
+    {
+      name: "EARTH",
+      color: "decoration-[#6D2ED5]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#6D2ED5] hover:underline-offset-[-45px]",
+    },
+    {
+      name: "MARS",
+      color: "decoration-[#D14C32]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#D14C32] hover:underline-offset-[-45px]",
+    },
+    {
+      name: "JUPITER",
+      color: "decoration-[#D83A34]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#D83A34] hover:underline-offset-[-45px]",
+    },
+    {
+      name: "SATURN",
+      color: "decoration-[#CD5120]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#CD5120] hover:underline-offset-[-45px]",
+    },
+    {
+      name: "URANUS",
+      color: "decoration-[#1EC1A2]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#1EC1A2] hover:underline-offset-[-45px]",
+    },
+    {
+      name: "NEPTUNE",
+      color: "decoration-[#2D68F0]",
+      hover:
+        "hover:underline hover:decoration-4 hover:decoration-[#2D68F0] hover:underline-offset-[-45px]",
+    },
+  ];

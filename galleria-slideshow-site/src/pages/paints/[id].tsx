fa1109f19350d07d83735c1bf25c711943af8e03
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-10-03 23:09:40 -0500

    tests for slugs mobile design

--- galleria-slideshow-site/src/pages/paints/[id].tsx
+++ galleria-slideshow-site/src/pages/paints/[id].tsx
@@ -2,7 +2,6 @@ import Head from "next/head";
 import { paints } from "../../data/data";
 import { Navbar } from "../../components/Navbar";
 import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from "next";
-import { BottomBar } from "../../components/BottomBar";
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const paths = paints.map((paint) => ({
@@ -31,18 +30,24 @@ const Paint = ({
         <link rel="icon" href="/favicon-32x32.png" />
       </Head>
       <Navbar />
-      <main>
-        <div className="relative">
-          <img src={allPaintsData.images.thumbnail} alt={allPaintsData.name} />
-          <h1 className="absolute bottom-0">{allPaintsData.name}</h1>
-          <p>{allPaintsData.artist.name}</p>
+      <main className="container mt-[24px] mx-auto flex flex-col items-center min-h-screen">
+        <div className="relative w-[327px]">
+          <img src={allPaintsData.images.hero.small} alt={allPaintsData.name} />
+          <div className="absolute w-[280px] h-[104px] bg-white -bottom-[52px] flex flex-col items-start justify-center">
+            <div className="ml-[24px]">
+              <h2 className="text-[24px] leading-[29px]">
+                {allPaintsData.name}
+              </h2>
+              <p>{allPaintsData.artist.name}</p>
+            </div>
+          </div>
         </div>
-        <h1>This the page for {allPaintsData.name}</h1>
+        <div className="relative mt-[98px] w-[327px]">
+          <img src={allPaintsData.artist.image} width="64" height="64" alt={allPaintsData.artist.name} className="absolute -top-[46px]"/>
+          <h1 className="absolute -top-[25px] right-[10px] text-[100px] text-[#F3F3F3] -z-10">{allPaintsData.year}</h1>
+          <p className="text-[#7D7D7D] text-[14px] leading-[28px] mt-[74px]">{allPaintsData.description}</p>
+        </div>
       </main>
-      <BottomBar
-        nameOfPaint={allPaintsData.name}
-        artist={allPaintsData.artist.name}
-      />
     </>
   );
 };

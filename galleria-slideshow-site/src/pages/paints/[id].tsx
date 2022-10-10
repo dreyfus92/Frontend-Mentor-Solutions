d3bbc6adef8c0d66ba31f1375d33fa84a4080fd4
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-10-10 12:51:08 -0500

    removed bottombar component

--- galleria-slideshow-site/src/pages/paints/[id].tsx
+++ galleria-slideshow-site/src/pages/paints/[id].tsx
@@ -105,7 +105,8 @@ const Paint = ({
           </p>
           <a
             href={allPaintsData.source}
-            className=" text-[9px] text-[#7D7D7D] underline my-[68px] self-start tracking-[1.92px]"
+            target="_blank"
+            className=" text-[9px] text-[#7D7D7D] underline self-start tracking-[1.92px]"
           >
             GO TO SOURCE
           </a>
@@ -114,15 +115,29 @@ const Paint = ({
       {/*BottomBar*/}
       <div className="relative h-[72px] border-t-[1px] border-[#E5E5E5] flex items-center justify-between">
         <div
-          className={`absolute top-0 h-[2px] ${tailwindWidth} bg-black`}
+          className={`absolute top-0 h-[1px] ${tailwindWidth} bg-red-600`}
         ></div>
         <div className="ml-[24px]">
           <p className="text-[14px] leading-[17px]">{allPaintsData.name}</p>
           <p className="text-[10px] mt-[9px]">{allPaintsData.artist.name}</p>
         </div>
         <div className="flex justify-between mr-[24px]">
-          <img src="/shared/icon-back-button.svg" />
-          <img src="/shared/icon-next-button.svg" className="ml-[24px]" />
+          <a
+            href={`/paints/${
+              allPaintsData.id > 1 ? allPaintsData.id - 1 : allPaintsData.id
+            }`}
+          >
+            <img src="/shared/icon-back-button.svg" />
+          </a>
+          <a
+            href={`/paints/${
+              allPaintsData.id < paintsLength
+                ? allPaintsData.id + 1
+                : allPaintsData.id
+            }`}
+          >
+            <img src="/shared/icon-next-button.svg" className="ml-[24px]" />
+          </a>
         </div>
       </div>
     </>

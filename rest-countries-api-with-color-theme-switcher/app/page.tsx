48d51d67de4976e41890ee9dc87b97bd11fbbdbd
Paul Valladares <85648028+dreyfus92@users.noreply.github.com>
2022-10-31 16:18:31 -0600

    added readme example and meta tag for responsive design

--- rest-countries-api-with-color-theme-switcher/app/page.tsx
+++ rest-countries-api-with-color-theme-switcher/app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="bg-blue-200">Hello from NextJS 13 with Tailwind</h1>
+      <h1 className="bg-blue-200 text-[14px]">
+        Hello from NextJS 13 with Tailwind
+      </h1>
     </div>
   );
 }
